f1ea64be3f880a66e97d96b0217c5591dbab2b58
Jose Antonio Maria de Rezende Silva <30808376+tonykingnz@users.noreply.github.com>
2018-07-31 12:03:47 -0300

    Add files via upload

---
+++ Interactive Dictionary/Application Main Folder/82.Accounting_for_non-existing_Words/Testing_and_Implenting/app1.py
@@ -0,0 +1,19 @@
+#made by @tonyKingNZ at Jul 21, 2018 7:01 alone without help and seeing any code
+#updated of other app
+
+
+import json #get json native library
+
+def word_meaning(word): #create a function to the word meaning and print
+    data = json.load(open("data.json")) #get data from .json file
+    if word in data: #check if word is in data
+        print("\n",data[word],"\n\n") #print word meanig
+    else: #if word not found write message
+        print("\n\nWord not found! Check your word\n\n")#message that word is not found
+
+word = input("\nWrite some word to see the meanig:\n\n") #get the word that want the word meaning
+word_meaning(word) #get word meanig
+
+
+#made by @tonyKingNZ at Jul 21, 2018 7:01 alone without help and seeing any code
+#update of other app
